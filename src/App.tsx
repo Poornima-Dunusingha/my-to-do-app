af272885bac39175b8a1a346355b247e2c5b5ba9
Poornima-Dunusingha <128152954+Poornima-Dunusingha@users.noreply.github.com>
2023-07-24 13:46:13 +0530

    test-commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+<<<<<<< HEAD
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+=======
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+>>>>>>> 00a8b7d58d527f2fcd23e6ffb28a2fa2f2b27bfe
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+<<<<<<< HEAD
+        <h1>HIIII</h1>
+=======
+>>>>>>> 00a8b7d58d527f2fcd23e6ffb28a2fa2f2b27bfe
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
